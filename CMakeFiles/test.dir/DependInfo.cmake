5c8e8e1c9b340675b8acd478ee743d219f2c52b2
IseanB <Isean4B@gmail.com>
2022-05-05 14:44:34 -0700

    Initial commit for new branch

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/ibhan003/lab-05---composite-pattern-smuku004-ibhan003-lab5/unit_test.cpp" "/home/csmajs/ibhan003/lab-05---composite-pattern-smuku004-ibhan003-lab5/CMakeFiles/test.dir/unit_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "gtest_disable_pthreads=ON"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/csmajs/ibhan003/lab-05---composite-pattern-smuku004-ibhan003-lab5/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "googletest/googletest/include"
+  "googletest/googletest"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
