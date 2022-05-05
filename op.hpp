130cbea2971245e366420291ec73713ecccccee3
IseanB <Isean4B@gmail.com>
2022-05-05 15:01:25 -0700

    Fixed OpEvaluateNonZero test error

--- op.hpp
+++ op.hpp
@@ -4,10 +4,12 @@
 #include "base.hpp"
 
 class Op : public Base {
+    private:
+	double storedValue = -1.0;
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base(), storedValue(value) { }
+        virtual double evaluate() { return storedValue; }
+        virtual std::string stringify() { return std::to_string(storedValue); }
 };
 
 #endif //__OP_HPP__
