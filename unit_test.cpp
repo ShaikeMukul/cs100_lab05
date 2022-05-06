a6fadf1bf8c768abe54facf8d7518eb71962f77e
IseanB <Isean4B@gmail.com>
2022-05-06 13:48:27 -0700

    Included other tests into ./test

--- unit_test.cpp
+++ unit_test.cpp
@@ -2,6 +2,9 @@
 
 #include "op_test.hpp"
 #include "Add_test.hpp"
+#include "Sub_test.hpp"
+#include "Pow_test.hpp"
+#include "Div_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
