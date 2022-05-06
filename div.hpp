ca34cfcf66557c1cc3640c75e6083d26cb2a42b4
ShaikeMukul <smuku004@ucr.edu>
2022-05-05 20:51:07 -0700

    div class added

---
+++ div.hpp
@@ -0,0 +1,30 @@
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+#include <iostream>
+#include <string>
+#include "base.hpp"
+
+using namespace std;
+
+class Div: public Base {
+   private:
+	Base* div1;
+	Base* div2;
+   public:
+	Div(Base* div_val1, Base* div_val2) : Base() {
+		div1 = div_val1;
+		div2 = div_val2;
+	}
+
+	virtual double evaluate() {
+		return (div1->evaluate() / div2->evaluate());
+	}
+
+	virtual std::string stringify() {
+		return("(" + div1->stringify() + " / " + div2->stringify() + ")");
+	}
+
+
+};
+
+#endif //__DIV_HPP__
