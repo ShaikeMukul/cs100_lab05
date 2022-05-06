738c380cb07296e60edf61637f43a3bccbe85e14
ShaikeMukul <smuku004@ucr.edu>
2022-05-05 22:45:46 -0700

    mult test added

--- mult_test.hpp
+++ mult_test.hpp
@@ -4,6 +4,13 @@
 #include "mult.hpp"
 #include "op.hpp"
 
+TEST(MultTest, MultEvaluateZero) {
+        Base* three = new Op(3);
+        Base* zero = new Op(0);
+        Mult* test = new Mult(three, zero);
+        EXPECT_EQ(test->evaluate(), 0);
+}
+
 TEST(MultTest, MultEvaluate) {
 	Base* three = new Op(3);
 	Base* four = new Op(4);
@@ -16,6 +23,13 @@ TEST(MultTest, MultEvaluateNeg) {
         Base* negfour = new Op(-4);
         Mult* test = new Mult(three, negfour);
         EXPECT_EQ(test->evaluate(), -12);
+}
+
+TEST(MultTest, MultStringifyZero) {
+        Base* three = new Op(3);
+        Base* zero = new Op(0);
+        Mult* test = new Mult(three, zero);
+        EXPECT_EQ(test->stringify(), "(3.000000 * 0.000000)");
 }
 
 TEST(MultTest, MultStringify) {
